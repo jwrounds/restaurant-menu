b13efc75c0f5919f266e46d9f258e2950e9f17f8
Jason <jwrounds@usfca.edu>
2021-05-14 12:08:12 -0500

    Added name field to MenuItem, and implemented Menu class

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,4 +1,23 @@
 package restaurant;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Menu {
+    protected ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private String lastUpdated;
+
+    public void addMenuItem(MenuItem item) {
+        if (!menuItems.contains(item)) {
+            menuItems.add(item);
+        }
+        lastUpdated = new Date().toString();
+    }
+    public void removeMenuItem(MenuItem item) {
+        menuItems.remove(item);
+        lastUpdated = new Date().toString();
+    }
+
+    public String getLastUpdated() { return lastUpdated; }
+    public void setLastUpdated() { lastUpdated = new Date().toString(); }
 }
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,16 +1,20 @@
 package restaurant;
 
 public class MenuItem {
+    private String name;
     protected double price;
     private String description;
     private String category;
 
-    public MenuItem(double price, String description, String category) {
+    public MenuItem(String name, double price, String description, String category) {
+        this.name = name;
         this.price = price;
         this.description = description;
         this.category = category;
     }
 
+    public String getName() { return name; }
+    public void setName(String aName) { name = aName; }
     public double getPrice() { return price; }
     public void setPrice(double aPrice) { price = aPrice; }
     public String getDescription() { return description; }
