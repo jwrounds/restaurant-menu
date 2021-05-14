973bfbf9acec07bec93dfaa8334cdd5be5eff5a6
Jason <jwrounds@usfca.edu>
2021-05-14 12:20:38 -0500

    Added test menu and items to Main

--- src/restaurant/Main.java
+++ src/restaurant/Main.java
@@ -3,6 +3,21 @@ package restaurant;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Menu newMenu = new Menu();
+	    MenuItem rickMac = new MenuItem("RickMac", 8.5, "A deep fried soft shell crab sandwiched between three buns with cheese and special sauce", "Sandwiches");
+	    MenuItem macAndCheese = new MenuItem("Mac and Cheese", 3.5, "Creamy, rich, three cheese mac", "Sides");
+	    MenuItem greens = new MenuItem("Collard Greens", 3, "Collard greens stewed with pork", "Sides");
+	    MenuItem fries = new MenuItem("Grace Fries", 2.5, "Seasoned, crinkle cut fries", "Sides");
+	    MenuItem milkShake = new MenuItem("Grace Milk Shake", 5.5, "Rich, creamy milk shake in vanilla, chocolate, or strawberry", "Drinks");
+
+	    newMenu.addMenuItem(rickMac);
+	    newMenu.addMenuItem(macAndCheese);
+	    newMenu.addMenuItem(greens);
+	    newMenu.addMenuItem(fries);
+	    newMenu.addMenuItem(milkShake);
+
+	    for (MenuItem item : newMenu.menuItems) {
+	        System.out.println("Name: " + item.getName() + ", Price: $" + item.getPrice());
+        }
     }
 }
